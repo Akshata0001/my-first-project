71dfb86c2ef0545858cb6e8e640b60334376133e
Akshata0001 <akshatametri714@gmail.com>
2026-04-18 23:03:08 +0530

    Add files via upload

---
+++ SortColors.java
@@ -0,0 +1,28 @@
+package prg1b3;
+	import java.util.ArrayList;
+	import java.util.Collections;
+
+	public class SortColors {
+	    public static void main(String[] args) {
+
+	        // Create ArrayList
+	        ArrayList<String> colors = new ArrayList<>();
+
+	        // Add colors
+	        colors.add("Red");
+	        colors.add("Blue");
+	        colors.add("Green");
+	        colors.add("Yellow");
+	        colors.add("Black");
+
+	        // Display original list
+	        System.out.println("Original List: " + colors);
+
+	        // Sort the ArrayList
+	        Collections.sort(colors);
+
+	        // Display sorted list
+	        System.out.println("Sorted List: " + colors);
+	    }
+	}
+
